9411d0b12974c785cee691f53ed5f82deaacb860
alexlourinha <54456493+alexlourinha@users.noreply.github.com>
2020-11-05 14:42:35 +0000

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 # If application.py
-gunicorn --bind=0.0.0.0 --timeout 600 application:app
+gunicorn --bind=0.0.0.0 --timeout 600 --chdir website application:app
 
 # If app.py
-gunicorn --bind=0.0.0.0 --timeout 600 app:app
+gunicorn --bind=0.0.0.0 --timeout 600 --chdir website app:app
